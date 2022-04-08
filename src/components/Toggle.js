cfebcc67a3a80bf8cda8faa9b4700c33329ea3f8
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-08 12:33:43 -0500

    commit tree 832b523493c547ad5e0fa564652166b14a53d32d

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [current, chngState] = useState(false);
+
+  function swap() {
+    chngState((current) => !current);
+  }
+  /*function chgBack(current){
+    if (current){
+      return "red"
+    } else {
+      return "white"
+    }
+  }*/
+
+  const color = current ? "red" : "white";
+
+  return <button onClick={swap} style={{ background: color}}>{current ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
